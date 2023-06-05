f1db399bd847537bd26ef34f3533c5abf372cc79
vu dinh thien 21-0-00557 <vudinhthien697@gmail.com>
2023-06-05 23:10:24 +0700

    vũ đình thiên

---
+++ major_view_model.dart
@@ -0,0 +1,28 @@
+import 'package:mvvm_sqlite_demo/dao/major_dao.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:mvvm_sqlite_demo/model/major.dart';
+
+class MajorViewModel extends Model {
+  static final MajorViewModel _instance = MajorViewModel._internal();
+  factory MajorViewModel() => _instance;
+  MajorDao _repo = MajorDao();
+  List<Major> majors = [];
+
+  MajorViewModel._internal() {
+    updateList();
+  }
+  void updateList() async {
+    majors = await _repo.getMajors();
+    notifyListeners();
+  }
+
+  void save(Major major) async {
+    major.id == 0 ? await _repo.insert(major) : await _repo.update(major);
+    updateList();
+  }
+
+  void delete(Major major) async {
+    await _repo.delete(major.id);
+    updateList();
+  }
+}

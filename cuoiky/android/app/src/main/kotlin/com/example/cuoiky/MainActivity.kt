1939b9cdf579c4311cb9564c95f2b523c29c258b
quang <nhatquang30082003@gmail.com>
2023-06-10 21:02:50 +0700

    quang

--- cuoiky/android/app/src/main/kotlin/com/example/cuoiky/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cuoiky
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- cuoiky/lib/model/major.dart
+++
@@ -1,6 +0,0 @@
-class Major {
-  int id;
-  String username;
-  String winlost;
-  Major({this.id = 0, required this.username, required this.winlost});
-}
